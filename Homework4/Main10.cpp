a35117b5f4f014e6f6f6ad8119d9bbd0347d80bd
Yun-Feng Lee <36137831+kyle1995lee@users.noreply.github.com>
2018-08-24 15:56:11 +0800

    Add files via upload

---
+++ Homework4/Main10.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "BinModel02.h"
+#include "Options06.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	BinModel Model;
+	if (Model.GetInputData() == 1) return 1;
+	Call Option1;
+	Option1.GetInputData();
+	cout << "European call option price = " << Option1.PriceByCRR(Model) << endl << endl;
+	Put Option2;
+	Option2.GetInputData();
+	cout << "European put option price = " << Option2.PriceByCRR(Model) << endl << endl;
+	BullSpread Option3;
+	Option3.GetInputData();
+	cout << "European bull spread price = " << Option3.PriceByCRR(Model) << endl << endl;
+	BearSpread Option4;
+	Option4.GetInputData();
+	cout << "European bull spread price = " << Option4.PriceByCRR(Model) << endl << endl;
+
+	system("pause");
+	return 0;
+}
+

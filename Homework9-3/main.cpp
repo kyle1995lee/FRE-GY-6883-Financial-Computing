5fb1bacaac9f50b9b24e12135c24a7eed5c5dddc
Yun-Feng Lee <36137831+kyle1995lee@users.noreply.github.com>
2018-08-24 16:22:49 +0800

    Add files via upload

---
+++ Homework9-3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "PathDepOption04.h"
+#include "GmtrAsianCall.h"
+using namespace std;
+int main()
+{
+	double S0 = 100.0, r = 0.03, sigma = 0.2;
+	BSModel Model(S0, r, sigma);
+
+	double T = 1.0 / 12.0, K = 100.0;
+	int m = 30;
+
+	ArthmAsianCall Option(T, K, m);
+	GmtrAsianCall  CVOption(T, K, m);
+
+	long N = 30000;
+	double epsilon = 0.001;
+	Option.PriceByVarRedMC(Model, N, CVOption, epsilon);
+	cout << "Arithmetic call price = " << Option.Price << endl
+		<< "Error = " << Option.PricingError << endl
+		<< "delta = " << Option.delta << endl << endl;
+
+	Option.PriceByMC(Model, N, epsilon);
+	cout << "Price by direct MC = " << Option.Price << endl
+		<< "Error = " << Option.PricingError << endl
+		<< "delta = " << Option.delta << endl;
+
+
+
+	system("pause");
+	return 0;
+}
+
+
